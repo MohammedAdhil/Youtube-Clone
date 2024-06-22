03e29330c5c6b93368b8a5ff542cbc7dc9b9b033
Adhilp <18gcs09@meaec.edu.in>
2024-06-22 12:33:46 +0400

    first

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+function SearchFeed() {
+  return (
+    <div>SearchFee
+        <Grid height={"500px"} bgcolor={"black"}></Grid>
+    </div>
+  )
+}
+
+export default SearchFeed
